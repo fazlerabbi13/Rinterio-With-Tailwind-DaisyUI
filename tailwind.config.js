ebc871df25f36bf329dc1449c9dfeaa9222dc738
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-16 01:10:25 +0600

    Fixed Form Stylings & Responsive & Some minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   theme: {
     extend: {
+       backgroundImage: {
+        'hero': "url('/assets')"
+      },
       colors: {
         primary: "#ff0000", // Custom primary color
       },
